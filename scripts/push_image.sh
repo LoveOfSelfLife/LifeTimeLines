de18a23e6447f782d3728f86235d2e00650422f5
DickKemp <dickkemp14@gmail.com>
2023-04-10 23:20:14 -0400

    refactor

--- scripts/push_image.sh
+++ scripts/push_image.sh
@@ -4,7 +4,7 @@
 # Environment variables:
 #
 #   CONTAINER_REGISTRY - The hostname of your container registry.
-#   REGISTRY_UN - User name for your container registry.
+#   REGISTRY_ID - User name for your container registry.
 #   REGISTRY_PW - Password for your container registry.
 #   VERSION - The version number to tag the images with.
 #
@@ -25,9 +25,9 @@ set -u # or set -o nounset
 : "$1"
 : "$CONTAINER_REGISTRY"
 : "$VERSION"
-: "$REGISTRY_UN"
+: "$REGISTRY_ID"
 : "$REGISTRY_PW"
 
 export APP=$1
-echo $REGISTRY_PW | docker login $CONTAINER_REGISTRY --username $REGISTRY_UN --password-stdin
+echo $REGISTRY_PW | docker login $CONTAINER_REGISTRY --username $REGISTRY_ID --password-stdin
 docker push $CONTAINER_REGISTRY/$APP:$VERSION
