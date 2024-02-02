481141875f10c4cb21460a34d0b39bb54fd34615
DickKemp <dickkemp14@gmail.com>
2024-02-01 21:21:22 -0500

    use a vault

--- common/google_credentials.py
+++ common/google_credentials.py
@@ -6,6 +6,7 @@ from flask_restx import Namespace, Resource
 from flask import session, request, url_for, redirect
 from google_auth_oauthlib.flow import Flow
 import common.encoder
+from common.vault import Vault
 
 GOOGLE_SCOPES = ['https://www.googleapis.com/auth/photoslibrary.readonly', \
                  'https://www.googleapis.com/auth/gmail.readonly', \
@@ -22,8 +23,10 @@ def get_config_from_secret():
 def get_credentials(scopes=GOOGLE_SCOPES):
     try:
         if 'credentials' in session and session['credentials'] is not None:
-            print('using credentials found in session')
+            print(f'using credentials found in session: {session["credentials"]}')
             session['credentials']['scopes'] = scopes
+            if refresh_token := session['credentials']['refresh_token']:
+                store_refresh_token(refresh_token)
             return google.oauth2.credentials.Credentials(**session['credentials'])
     except:
         pass
@@ -46,32 +49,33 @@ def get_credentials(scopes=GOOGLE_SCOPES):
     return None
 
 def get_refresh_token():
-    if refresh_token_store := os.getenv('REFRESH_TOKEN_STORE', None):
-        try:
-            with open(refresh_token_store, 'r') as rtf:
-                refresh_token = rtf.readline()
-                return refresh_token
-        except OSError:
-            return None
+    kv = Vault()
+    if refresh_token := kv.get_secret_from_vault("refreshtoken"):
+        print("found refresh_token in keyvault")
+        return refresh_token
+    
+    # if refresh_token_store := os.getenv('REFRESH_TOKEN_STORE', None):
+    #     try:
+    #         with open(refresh_token_store, 'r') as rtf:
+    #             refresh_token = rtf.readline()
+    #             return refresh_token
+    #     except OSError:
+    #         return None
+
     return None
 
-def store_credentials(credentials):
-    refresh_creds = credentials.refresh_token
-
-    session['credentials'] = {
-        'token': credentials.token,
-        'refresh_token': refresh_creds,
-        'token_uri': credentials.token_uri,
-        'client_id': credentials.client_id,
-        'client_secret': credentials.client_secret,
-        'scopes': credentials.scopes}
-    if not refresh_creds:
+def store_refresh_token(refresh_token):
+    if not refresh_token:
         print('refresh token is None - cannot store refresh token')
     else:
-        if refresh_token_store := os.getenv('REFRESH_TOKEN_STORE', None):
-            print('storing refresh token to file system')
-            with open(refresh_token_store, 'w') as rtf:
-                rtf.write(credentials.refresh_token)
+        print('storing refresh token to keyvault')
+        kv = Vault()
+        kv.set_secret_to_vault("refreshtoken", str(refresh_token))
+
+        # if refresh_token_store := os.getenv('REFRESH_TOKEN_STORE', None):
+        #     print('storing refresh token to file system')
+        #     with open(refresh_token_store, 'w') as rtf:
+        #         rtf.write(refresh_token)
 
 def google_doauth(url_auth_shorthand):
     print('in /doauth')
@@ -131,7 +135,15 @@ def google_auth(url_auth_shorthand, url_whendone_shorthand):
     flow.fetch_token(authorization_response=authorization_response)
 
     # Store the credentials in the session & store refresh token in storage
-    store_credentials(flow.credentials)
+    session['credentials'] = {
+        'token': flow.credentials.token,
+        'refresh_token': flow.credentials.refresh_token,
+        'token_uri': flow.credentials.token_uri,
+        'client_id': flow.credentials.client_id,
+        'client_secret': flow.credentials.client_secret,
+        'scopes': flow.credentials.scopes}    
+    
+    store_refresh_token(flow.credentials.refresh_token)
 
     return redirect(url_for(url_whendone_shorthand))
 
--- common/vault.py
+++ common/vault.py
@@ -1,22 +1,41 @@
 import os
 from azure.keyvault.secrets import SecretClient
-from azure.identity import DefaultAzureCredential
+from azure.identity import DefaultAzureCredential, ClientSecretCredential
+
+def get_creds_from_env():
+    client_id = os.getenv('AZURE_CLIENT_ID', None)
+    client_secret = os.getenv('AZURE_CLIENT_SECRET', None)
+    tenant_id = os.getenv('TENANT_ID', None)
+    if client_id and client_secret and tenant_id:
+        return { "tenant_id": tenant_id, "client_id" : client_id, "client_secret" : client_secret }
+    else:
+        return None
 
 class Vault:
 
-    def __init__(self, vault_name):
+    def __init__(self, vault_name = "lifetimelines-secrets-1"):
         self.vault_name = vault_name
-        self.KVUri = f"https://{vault_name}.vault.azure.net"
-        self.credential = DefaultAzureCredential()
-        self.client = SecretClient(vault_url=self.KVUri, credential=self.credential)
+        self.key_vault_uri = f"https://{vault_name}.vault.azure.net"
+        creds = get_creds_from_env()
+        if creds:
+            self.credential = ClientSecretCredential(creds["tenant_id"], creds["client_id"], creds["client_secret"])
+        else:
+            self.credential = DefaultAzureCredential()
+        self.client = SecretClient(vault_url=self.key_vault_uri, credential=self.credential)
         
     def get_secret_from_vault(self, secret_name):
-        val = self.client.get_secret(secret_name)
-        return val
+        try:
+            val = self.client.get_secret(secret_name)
+            return val.value
+        except:
+            return None
 
     def set_secret_to_vault(self, secret_name, secret_value):
-        val = self.client.set_secret(secret_name, secret_value)
-        return val
+        try:
+            self.client.set_secret(secret_name, secret_value)
+        except:
+            print('exception when attempting to set secret')
+
 
 
 
