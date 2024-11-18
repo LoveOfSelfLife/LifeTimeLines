26af5d325a3c8aa5d758bb7f6f99a6cab44e93f4
DickKemp <dickkemp14@gmail.com>
2024-11-18 17:11:18 -0500

    debugging

--- common/orchestration/modules/zippy_module.py
+++ common/orchestration/modules/zippy_module.py
@@ -5,18 +5,22 @@ import json
 
 def zip_crawler(path_to_zipfile, output_file, token=None, instance_id=None):
     logging.info(f"Starting zip_crawler(path_to_zipfile={path_to_zipfile})")
-
     try:
         json_file_names, non_json_file_names = zip_crawl(path_to_zipfile)
-        json_list = zip_json_extractor(path_to_zipfile, json_file_names)
-        n=0
+
         with open(output_file, 'w') as f:
-            for item in json_list:
-                logging.info(f"processing file #{n}: {output_file}")
-                f.write('[\n' if n==0 else ',\n')
-                n += 1
-                f.write(f"{json.dumps(item, indent=4)}")
-            f.write(']\n')
+            for jname in json_file_names:
+                f.write(f"{jname}\n")
+
+        # json_list = zip_json_extractor(path_to_zipfile, json_file_names)
+        # n=0
+        # with open(output_file, 'w') as f:
+        #     for item in json_list:
+        #         logging.info(f"processing file #{n}: {item}")
+        #         f.write('[\n' if n==0 else ',\n')
+        #         n += 1
+        #         f.write(f"{json.dumps(item, indent=4)}")
+        #     f.write(']\n')
     except Exception as e:
         logging.exception(f"Error occurred during zip_crawler: {e}")
         raise Exception(f"Error occurred during zip_crawler: {e}")
