7b631fb3c2f28dc77248bb1448f00e65f7ec5e0f
DickKemp <dickkemp14@gmail.com>
2025-04-15 15:57:41 -0400

    debuggin

--- services/periodic-time-event-publisher/appfactory.py
+++ services/periodic-time-event-publisher/appfactory.py
@@ -8,17 +8,17 @@ from common.fitness.outbound_event_queue import OutboundEventQueue
 
 def main() -> None:
 
-    if connection_string := os.getenv("AZURE_STORAGETABLE_CONNECTIONSTRING") is None:
+    if (connection_string := os.getenv("AZURE_STORAGETABLE_CONNECTIONSTRING")) is None:
         raise Exception(f'You attempted to run the container without providing the AZURE_STORAGE_CONNECTION_STRING')
 
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)  # Adjust level as needed (e.g., DEBUG, INFO, WARNING)
     logger = logging.getLogger(__name__)
-    
+    logging.info(f'AZURE_STORAGE_CONNECTION_STRING: <{connection_string[0:100]}...>')    
     outbound_event_queue_client = OutboundEventQueue.get_queue_client(connection_string)
     logger.info(f'Client created for: {OutboundEventQueue.queue_name}')
 
     outbound_event_queue_client.send_message(json.dumps({"event": "periodic-time-event", "timestamp": datetime.now().isoformat()}))
-    
+
     logger.info(f'Enqueued periodic time event message')
     logger.info(f'exiting container gracefully')
     exit(0)
